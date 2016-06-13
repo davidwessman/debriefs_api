b93507eecef72764c72efdd4f8c45f78083a90ed
davidwessman <david.wesmn@gmail.com>
2016-06-13 17:03:05 +0200

    User: implements update endpoint

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -66,4 +66,49 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       end
     end
   end
+
+  describe 'PATCH #update' do
+    context 'successfully updated' do
+      it 'renders json represenation of updated user' do
+        user = create(:user, email: 'david@notify.dev')
+        patch(:update, id: user.to_param,
+                       user: { email: 'arthur@notify.dev' },
+                       format: :json)
+
+       user_response = JSON.parse(response.body, symbolize_names: true)
+       expect(user_response[:email]).to eq('arthur@notify.dev')
+      end
+
+      it 'responds with 200' do
+        user = create(:user, email: 'david@notify.dev')
+        patch(:update, id: user.to_param,
+                       user: { email: 'arthur@notify.dev' },
+                       format: :json)
+
+        is_expected.to(respond_with(200))
+      end
+    end
+
+    context 'not updated' do
+      it 'renders and errors json' do
+        user = create(:user, email: 'david@notify.dev')
+        patch(:update, id: user.to_param,
+                       user: { email: '' },
+                       format: :json)
+
+       user_response = JSON.parse(response.body, symbolize_names: true)
+       expect(user_response).to have_key(:errors)
+       expect(user_response[:errors][:email]).to include("can't be blank")
+      end
+
+      it 'responds with 422' do
+        user = create(:user, email: 'david@notify.dev')
+        patch(:update, id: user.to_param,
+                       user: { email: '' },
+                       format: :json)
+
+        is_expected.to(respond_with(422))
+      end
+    end
+  end
 end
