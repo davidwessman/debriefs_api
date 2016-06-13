f64eab64e1f0f55a2319fb4b41440efe3468fcba
davidwessman <david.wesmn@gmail.com>
2016-06-13 16:52:53 +0200

    User create endpoint
    Adding specs and controller action for creating users

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -21,4 +21,49 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       is_expected.to(respond_with(200))
     end
   end
+
+  describe 'POST #create' do
+    context 'successfully created' do
+      it 'renders the json-representation for the user record created' do
+        attributes = {email: 'david@notify.dev',
+                      password: '12345678',
+                      password_confirmation: '12345678'}
+        post(:create, user: attributes, format: :json)
+
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eq('david@notify.dev')
+      end
+
+      it 'responds with 201' do
+        attributes = {email: 'david@notify.dev',
+                      password: '12345678',
+                      password_confirmation: '12345678'}
+        post(:create, user: attributes, format: :json)
+
+        is_expected.to(respond_with(201))
+      end
+    end
+
+    context 'not created' do
+      it 'renders and json of errors' do
+        attributes = {email: '',
+                      password: '12345678',
+                      password_confirmation: '12345678'}
+        post(:create, user: attributes, format: :json)
+
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+        expect(user_response[:errors][:email]).to include("can't be blank")
+      end
+
+      it 'responds with 422' do
+        attributes = {email: '',
+                      password: '12345678',
+                      password_confirmation: '12345678'}
+        post(:create, user: attributes, format: :json)
+
+        is_expected.to(respond_with(422))
+      end
+    end
+  end
 end
