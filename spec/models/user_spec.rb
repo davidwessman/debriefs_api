50e4f7369b50aff40c93091656bc36375427d078
davidwessman <david.wesmn@gmail.com>
2016-06-13 19:32:33 +0200

    Adds auth_token to User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,9 +6,32 @@ RSpec.describe User, type: :model do
   it { is_expected.to respond_to(:email) }
   it { is_expected.to respond_to(:password) }
   it { is_expected.to respond_to(:password_confirmation) }
+  it { is_expected.to respond_to(:auth_token) }
 
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
   it { is_expected.to validate_confirmation_of(:password) }
   it { is_expected.to allow_value('example@domain.com').for(:email) }
+  it { is_expected.to validate_uniqueness_of(:auth_token) }
+
+  describe '#generate_authentication_token!' do
+    it 'generates a unique token' do
+      user = create(:user)
+      allow(Devise).to receive(:friendly_token).and_return('uniquetoken1337')
+
+      user.generate_authentication_token!
+      expect(user.auth_token).to eq('uniquetoken1337')
+    end
+
+    it 'generates different than current' do
+      user = create(:user, auth_token: 'notuniquetoken1337')
+      allow(Devise).to receive(:friendly_token).and_return('notuniquetoken1337', 'uniquetoken1337')
+
+      user.generate_authentication_token!
+      expect(user.auth_token).to eq('uniquetoken1337')
+      begin
+        self.auth_token = Devise.friendly_token
+      end while self.class.exists?(auth_token: auth_token)
+    end
+  end
 end
